3bb31ab47f1c920c11459e82f6b1e322df3a20b9
Onat <53895969+onattech@users.noreply.github.com>
2022-02-02 21:38:20 +0300

    Added live chip

--- src/Components/Kuveyt.jsx
+++ src/Components/Kuveyt.jsx
@@ -2,21 +2,13 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
 import Typography from './CustomTypography';
+import Chip from '@mui/material/Chip';
 import Kv from '../kuveyt-turk.png';
-import { useEffect, useState } from 'react';
+import useGetExRate from '../Hooks/useGetExRate';
 
 export default function Kuveyt({ state: { usdSent, exRateAfterFees, tryReceived } }) {
-    const [currentExRate, setCurrentExRate] = useState('');
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            fetch('https://go-proxy-kuveyt.herokuapp.com/')
-                .then((result) => result.text())
-                .then((content) => setCurrentExRate(extractUSD(content)));
-        }, 5000);
-
-        return () => clearInterval(interval);
-    }, []);
+    // Custom hook
+    const [currentExRate, isLive] = useGetExRate();
 
     return (
         <Card sx={{ width: 500 }}>
@@ -24,19 +16,22 @@ export default function Kuveyt({ state: { usdSent, exRateAfterFees, tryReceived 
                 <img alt='KuveytTurk' src={Kv} style={{ width: 125 }} />
 
                 {/* Current USD/TRY exchange rate */}
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography variant='body2'>Current USD/TRY exchange rate</Typography>
-                    <Typography variant='number'>{currentExRate[1]}</Typography>
+                    <Typography variant='number'>
+                        {isLive ? <Chip label='Live ●' variant='outlined' size='small' sx={{ background: 'white', color: 'green', mr: 1 }} /> : null}
+                        {currentExRate[1]}
+                    </Typography>
                 </Box>
 
                 {/* Exchange rate to break even */}
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography variant='body2'>Exchange rate to break even</Typography>
                     {exRateAfterFees ? <Typography variant='number'>{(exRateAfterFees * 0.998).toFixed(4)}</Typography> : null}
                 </Box>
 
                 {/* Exchange rate difference */}
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography variant='body2'>Exchange rate difference</Typography>
                     {exRateAfterFees ? (
                         <Typography variant='number' color={getExRateDiff(currentExRate[1], exRateAfterFees) > 0 ? '#2ead4b' : 'red'}>
@@ -46,13 +41,13 @@ export default function Kuveyt({ state: { usdSent, exRateAfterFees, tryReceived 
                 </Box>
 
                 {/* USD at current exchange rate */}
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography variant='body2'>USD at current exchange rate</Typography>
                     {!isNaN(tryReceived) ? <Typography variant='number'>{((tryReceived / currentExRate[1]) * 0.998).toFixed(2)} USD</Typography> : null}
                 </Box>
 
                 {/* Gain */}
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography variant='body2'>Gain</Typography>
                     {!isNaN(tryReceived) ? (
                         <Typography variant='number' color={getGain(tryReceived, currentExRate, usdSent) > 0 ? '#2ead4b' : 'red'}>
@@ -63,21 +58,12 @@ export default function Kuveyt({ state: { usdSent, exRateAfterFees, tryReceived 
             </CardContent>
         </Card>
     );
-}
 
-function extractUSD(text) {
-    const idx = text.indexOf('USD (Amerikan Doları)');
-    const section = text.substring(idx, idx + 800);
-    const rate = section.match(/\d\d,\d\d\d\d/g);
-    const rateClean = rate.map((a) => (a.replace(',', '.') * 1).toFixed(4));
+    function getExRateDiff(c, e) {
+        return ((c - e * 0.998) * -1).toFixed(2);
+    }
 
-    return rateClean;
-}
-
-function getExRateDiff(c, e) {
-    return ((c - e * 0.998) * -1).toFixed(2);
-}
-
-function getGain(amountReceivedTr, currentExRate, dollarSent) {
-    return ((amountReceivedTr / currentExRate[1]) * 0.998 - dollarSent).toFixed(2);
+    function getGain(amountReceivedTr, currentExRate, dollarSent) {
+        return ((amountReceivedTr / currentExRate[1]) * 0.998 - dollarSent).toFixed(2);
+    }
 }
---
+++ src/Hooks/useGetExRate.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+export default function useGetExRate() {
+    const [currentExRate, setCurrentExRate] = useState([]);
+    const [isLive, setLive] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetch('https://go-proxy-kuveyt.herokuapp.com/')
+                .then((result) => result.text())
+                .then((content) => {
+                    setLive(true);
+                    setCurrentExRate(extractUSD(content));
+                })
+                .catch((err) => {
+                    setLive(false);
+                    console.log(err);
+                });
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return [currentExRate, isLive];
+}
+
+function extractUSD(text) {
+    const idx = text.indexOf('USD (Amerikan Doları)');
+    const section = text.substring(idx, idx + 800);
+    const rate = section.match(/\d\d,\d\d\d\d/g);
+    const rateClean = rate.map((a) => (a.replace(',', '.') * 1).toFixed(4));
+
+    return rateClean;
+}
